7bd3f19b2ce1f7d078b3a471868e9f8d191466f5
Franck Samson <franck.samson@inra.fr>
2025-03-06 11:04:11 +0100

    zerzer

--- bot.py
+++ bot.py
@@ -310,7 +310,6 @@ async def blast(interaction:discord.Interaction,
                  'sequence': sequence,
                  'speciesId': speciesId,
                  'speciesName' : speciesName,
-                 'inter': 'False',
                  'gender': 'Plant'
                  }
     # Mettre en attente discord
